cdac1ae855875ee3263bde2be1bf25fdbadab1bf
maftunamaqsudova <maftuna.starr@gmail.com>
2025-12-06 12:57:01 +0500

    3darshw

---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,430 @@
+﻿namespace Homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#1
+
+            //            for (int i = 1; i <= 20; i++)
+            //            {
+            //                Console.Write($"{i}, ");
+            //            }
+
+            //#2
+
+            for (int i = 25; i <= 100; i++)
+            {
+                Console.Write($"{i} ");
+            }
+
+            //#3
+
+            //            for (int i = 12; i <= 200; i++)
+            //            {
+            //                Console.Write($"{i} ");
+            //            }
+
+            //#4
+
+            //            for (int i = 15; i >= 2; i--)
+            //            {
+            //                Console.Write($"{i}, ");
+            //            }
+
+            //#5
+
+            //            for (int i = 120; i >= 23; i--)
+            //            {
+            //                Console.Write($"{i}, ");
+            //            }
+
+            //#6
+
+            //            for (int i = 10; i <= 50; i++)
+            //            {
+            //                if (i % 2 != 0)
+            //                {
+            //                    Console.Write($"{i}, ");
+            //                }
+            //            }
+
+            //#7
+
+            //            for (int i = 120; i <= 250; i++)
+            //            {
+            //                if (i % 2 == 0)
+            //                {
+            //                    Console.Write($"{i}, ");
+            //                }
+            //            }
+
+            //#8
+
+            //            for (int i = 100; i >= 50; i--)
+            //            {
+            //                if (i % 2 != 0)
+            //                {
+            //                    Console.Write($"{i}, ");
+            //                }
+            //            }
+
+            //#9
+
+            //            for (int i = 80; i >= 20; i--)
+            //            {
+            //                if (i % 2 != 0)
+            //                {
+            //                    Console.Write($"{i}, ");
+            //                }
+            //            }
+
+            //            Console.Write("1chi sonni kiriting: ");
+            //            int firstNum = int.Parse(Console.ReadLine());
+            //            Console.Write("2chi sonni kiriting: ");
+            //            int secondNum = int.Parse(Console.ReadLine());
+
+            //            for (int i = firstNum; i <= secondNum; i++)
+            //            {
+            //                if (i % 5 == 0)
+            //                {
+            //                    Console.Write($"{i}, ");
+            //                }
+            //            }
+
+            //#11
+
+            //            Console.Write("1-sonni kiriting: ");
+            //            int FirstNum = int.Parse(Console.ReadLine());
+            //            Console.Write("2-sonni kiriting: ");
+            //            int SecondNum = int.Parse(Console.ReadLine());
+
+            //            for (int i = FirstNum; i < SecondNum; i++)
+            //            {
+            //                if (i % 4 == 0 && i % 7 == 0)
+            //                {
+            //                    Console.Write($"{i}, ");
+            //                }
+            //            }
+            //        }
+
+            //#12
+
+            //        Console.Write("Enter 1st number:");
+            //            int FirstNum = int.Parse(Console.ReadLine());
+            //        Console.Write("Enter 2nd number:");
+            //            int SecondNum = int.Parse(Console.ReadLine());
+
+            //        int counter = 0;
+
+            //            for(int i=FirstNum; i<SecondNum; i++)
+            //            {
+            //                if (i % 8 == 0) counter++;
+            //            }
+            //    Console.Write($"{counter}");
+
+            //            #13
+
+            //            Console.Write("Enter a number: ");
+            //            int FirstNum = int.Parse(Console.ReadLine());
+            //    Console.Write("Enter a number: ");
+            //            int SecondNum = int.Parse(Console.ReadLine());
+
+            //            for(int i=SecondNum; i > FirstNum; i--)
+            //            {
+            //                if(i % 9 == 0 || i % 5 == 0)
+            //                {
+            //                    Console.Write($"{i}, ");
+            //                }
+            //            }
+
+            //            #14
+            //            int res = 0;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        res += i;
+            //    }
+            //}
+            //Console.Write(res);
+
+            //#15
+
+            //for (int i = 100; i < 1000; i++)
+            //{
+            //    int a = i / 100;
+            //    int b = i % 100 / 10;
+            //    int c = i % 10;
+            //    if (a + b + c >= 20)
+            //    {
+            //        Console.Write($"{i}, ");
+            //    }
+            //}
+
+            //#16
+
+            //for (int i = 100; i <= 999; i++)
+            //{
+            //    int a = i / 100;
+            //    int b = i % 100 / 10;
+            //    int c = i % 10;
+            //    if (a == c)
+            //    {
+            //        Console.Write($"{i}, ");
+            //    }
+            //}
+
+            //#17
+            //int counter = 0;
+            //int res = 0;
+            //for (int i = 10; i < 100; i++)
+            //{
+            //    int a = i / 10;
+            //    int b = i % 10;
+            //    if (a * b == 12)
+            //    {
+            //        res += i;
+            //        counter++;
+            //    }
+
+            //}
+            //Console.WriteLine(counter);
+            //Console.WriteLine(res);
+
+            //#18
+
+            //for (int i = 100; i < 1000; i++)
+            //{
+            //    int b = i % 100 / 10;
+            //    if (b == 2 || b == 5)
+            //    {
+            //        Console.Write($"{i}, ");
+            //    }
+            //}
+
+            //#19 
+
+            //Console.Write("Enter a number: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < num; i++)
+            //{
+            //    Console.Write($"{num} ");
+            //}
+
+            //#20
+            //Console.Write("Enter a number: ");
+            //int FirstNum = int.Parse(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //int SecondNum = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < SecondNum; i++)
+            //{
+            //    Console.Write(FirstNum);
+            //}
+
+            //#21
+
+            //Console.Write("Enter a number: ");
+            //int FirstNum = int.Parse(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //int SecondNum = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < FirstNum; i++)
+            //{
+            //    Console.Write($"{SecondNum}");
+            //}
+
+            //#22
+
+            //Console.Write("Enter a number: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    if (num % i == 0)
+            //    {
+            //        Console.Write($"{i}, ");
+            //    }
+            //}
+
+            //#23
+
+            //Console.Write("Enter a number:");
+            //int num = int.Parse(Console.ReadLine());
+
+            //int counter = 0;
+
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    if (num % i == 0)
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //Console.Write(counter);
+
+            //#24
+
+            //Console.Write("Enter a number: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //int res = 0;
+
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    if (num % i == 0)
+            //    {
+            //        res += i;
+            //    }
+            //}
+            //Console.WriteLine(res);
+
+            //#25
+
+            //Console.Write("Enter a number: ");
+            //int num = int.Parse(Console.ReadLine());
+            //int counter = 0;
+
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    if (num % i == 0)
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //if (counter == 2)
+            //{
+            //    Console.Write(true);
+            //}
+            //else
+            //{
+            //    Console.Write(false);
+            //}
+
+            //#26
+
+            //Console.Write("Enter a number: ");
+            //int FirstNum = int.Parse(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //int SecondNum = int.Parse(Console.ReadLine());
+
+            //int counter1 = 0;
+            //int counter2 = 0;
+
+            //for (int i = 1; i <= FirstNum; i++)
+            //{
+            //    if (FirstNum % i == 0)
+            //    {
+            //        counter1++;
+            //    }
+            //}
+            //for (int i = 1; i <= SecondNum; i++)
+            //{
+            //    if (SecondNum % i == 0)
+            //    {
+            //        counter2++;
+            //    }
+            //}
+            //if (counter1 == 2 && counter2 == 2)
+            //{
+            //    Console.WriteLine($"{FirstNum + SecondNum}");
+            //}
+            //else
+            //{
+            //    Console.Write($"{FirstNum * SecondNum}");
+            //}
+
+            //#27
+
+            //1kg = 7000, 3 - 60;
+
+            //int kg = 7000;
+            //int res;
+
+            //for (int i = 3; i <= 60; i++)
+            //{
+            //    Console.WriteLine($"{i * kg}, ");
+            //}
+
+            //#28
+
+            //Console.Write("Enter a number: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //for (int i = 23; i < num; i++)
+            //{
+            //    if (i % 3 != 0)
+            //    {
+            //        Console.Write($"{i} ");
+            //    }
+            //}
+            //for (int i = num; i < 23; i--)
+            //{
+            //    if (i % 3 != 0)
+            //    {
+            //        Console.Write($"{i} )");
+            //    }
+            //}
+
+            //#29
+
+            //Console.Write("Enter a number: ");
+            //int FirstNum = int.Parse(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //int SecondNum = int.Parse(Console.ReadLine());
+
+            //int counter1 = 0;
+            //int counter2 = 0;
+
+            //for (int i = 0; i <= FirstNum; i++)
+            //{
+            //    if (FirstNum % i == 0)
+            //    {
+            //        counter1++;
+            //    }
+            //}
+            //for (int i = 0; i <= SecondNum; i++)
+            //{
+            //    if (SecondNum % i == 0)
+            //    {
+            //        counter2++;
+            //    }
+            //}
+            //if (counter1 > counter2)
+            //{
+            //    Console.WriteLine("1-sonni bo'luvchilari ko'p.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("2-chi sonni bo'luvchilari ko'p.");
+            //}
+
+            //#30
+
+            //Console.Write("Enter a number: ");
+            //int FirstNum = int.Parse(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //int SecondNum = int.Parse(Console.ReadLine());
+
+            //int counter = 0;
+            //for (int i = FirstNum; i < SecondNum; i++)
+            //{
+            //    if (FirstNum < i)
+            //        counter += i;
+            //}
+            //Console.Write(counter);
+
+            //#31
+
+            //Console.Write("1kg konfet narxini kiriting: ");
+            //int kg = int.Parse(Console.ReadLine());
+
+            //for (float i = 1; i < 2; i++)
+            //{
+            //    Console.Write($"{i * kg} ");
+            //}
+        }
+    }
+}
